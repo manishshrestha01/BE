5a6a6ebd135b54b29656832d8c3f211e73ccf8ef
manishshrestha01 <shresthamanish8080@gmail.com>
2026-09-07 03:34:33 +0545

    feat: complete Computer Workshop unit notes (IV-VIII), rename Read Article to Read Notes, solid navbar (white/black)

--- src/components/Blog/BlogSemester.jsx
+++ src/components/Blog/BlogSemester.jsx
@@ -95,7 +95,7 @@ const BlogSemester = ({ semesterId }) => {
                     and practice set.
                   </p>
                   <Link href={subject.urlPath} className="blog-btn">
-                    Read Article
+                    Read Notes
                   </Link>
                 </article>
               ))}
